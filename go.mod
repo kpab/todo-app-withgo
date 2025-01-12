ad7c355add4582e743a9e8e0e87fbb45514c240d
w.kashiwagi <kpablow1122@gmail.com>
2025-01-12 18:52:25 +0900

    ユーザーの作成機能

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module todo-app-withgo
 
 go 1.23.2
 
-require gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"todo-app-withgo/config"
+	"todo-app-withgo/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	fmt.Println(models.Db)
 
-	log.Println("test")
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
 }
