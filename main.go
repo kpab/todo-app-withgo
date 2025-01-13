5999f9cf92a45bd19cbdc00c954ac81f4654ec47
w.kashiwagi <kpablow1122@gmail.com>
2025-01-13 15:43:55 +0900

    userの取得、更新

--- main.go
+++ main.go
@@ -6,13 +6,23 @@ import (
 )
 
 func main() {
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
 	fmt.Println(u)
 
-	u.CreateUser()
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 }
