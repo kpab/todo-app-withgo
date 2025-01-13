39678cf468c31cfc8d366bb063bef1e33003f251
w.kashiwagi <kpablow1122@gmail.com>
2025-01-13 16:10:57 +0900

    todonの作成、取得

---
+++ app/models/todos.go
@@ -0,0 +1,60 @@
+package models
+
+import (
+	"log"
+	"time"
+)
+
+type Todo struct {
+	ID int
+	Content string
+	UserId int
+	CreatedAt time.Time	
+}
+
+func (u *User) CreateTodo(content string) (err error) {
+	cmd := `insert into todos (
+		content, 
+		user_id, 
+		created_at) values (?, ?, ?)`
+	
+	_, err = Db.Exec(cmd, content, u.ID, time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func GetTodo(id int) (todo Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos
+		where id = ?`
+	todo = Todo{}
+
+	err = Db.QueryRow(cmd, id).Scan(&todo.ID, &todo.Content, &todo.UserId, &todo.CreatedAt)
+	
+	return todo, err
+}
+
+func GetTodos() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos`
+	rows, err := Db.Query(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(
+			&todo.ID, 
+			&todo.Content, 
+			&todo.UserId, 
+			&todo.CreatedAt)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close()
+
+	return todos, err
+}
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	// fmt.Println(models.Db)
+	fmt.Println(models.Db)
 
 	// u := &models.User{}
 	// u.Name = "test"
@@ -16,19 +16,33 @@ func main() {
 
 	// u.CreateUser()
 
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
+	// u, _ := models.GetUser(1)
+	// fmt.Println(u)
 
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
 
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u.DeleteUser()
+	// u.DeleteUser()
 
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
+	// user, _ := models.GetUser(2)
+	// fmt.Println(u)
+	// user.CreateTodo("First Todo")
+
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("Second Todo")
+
+	todos, _ := models.GetTodos()
+	for _, v := range todos {
+		fmt.Println(v)
+	}
 }
