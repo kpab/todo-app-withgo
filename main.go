fd8f5b8ec3e1866c66ff92d9db7557ff98a008a0
w.kashiwagi <kpablow1122@gmail.com>
2025-01-13 16:18:12 +0900

    todoの取得（user毎）

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
 
 	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
 	// u.PassWord = "testtest"
 	// fmt.Println(u)
 
@@ -35,13 +35,19 @@ func main() {
 	// fmt.Println(u)
 	// user.CreateTodo("First Todo")
 
-	t, _ := models.GetTodo(1)
-	fmt.Println(t)
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("Second Todo")
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("Third Todo")
 
-	todos, _ := models.GetTodos()
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	user2, _ := models.GetUser(3)
+	todos, _ := user2.GetTodosByUser()
 	for _, v := range todos {
 		fmt.Println(v)
 	}
